d9b522974f79895c95f598f7b115168e44499ea8
H.Yamada <ymd_h@outlook.jp>
2025-03-06 22:09:42 +0900

    Set UV_PYTHON_PREFERENCE=only-managed
    Disable system (nix) python.

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@
 
  pkgs.mkShell (extraArgs // {
    NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (libraries ++ extraLibraries);
+   UV_PYTHON_PREFERENCE = "only-managed";
    HATCH_ENV_TYPE_VIRTUAL_UV_PATH = "${pkgs.uv}/bin/uv";
 
    packages = [ pkgs.uv ] ++ extraPackages;
